2073081db82cd10f3ba72e71e7dd8119370f0413
Kossy Ugochukwu <kossyugochukwu@gmail.com>
2023-04-20 00:50:47 +0100

    fix error handling

--- frontend/src/utils/handle-error.ts
+++ frontend/src/utils/handle-error.ts
@@ -1,14 +1,22 @@
-import { HTTPError } from 'ky';
+import { HTTPError as KyError } from 'ky';
+import { HTTPError as GotError } from 'got';
 
 export const errorToResponse = async (error: unknown) => {
   let message = 'Server Error';
   let status = 500;
 
-  if (error instanceof HTTPError) {
+  if (error instanceof KyError) {
     const response = await error.response.json();
 
     message = response.message;
     status = error.response.status;
+  }
+
+  if (error instanceof GotError) {
+    const response = await JSON.parse(error.response.body as string);
+
+    message = response.message;
+    status = error.response.statusCode;
   }
 
   return new Response(JSON.stringify({ success: false, message }), { status });
